25e4f52189399d386e0e3d0d24436e4a5b051d09
Alp Sakaci <alpsakaci@protonmail.com>
2021-05-14 20:38:54 +0300

    OAuth2.0 login sample

---
+++ src/main/java/com/example/spotifyOauth/SpotifyOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyOauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotifyOauth/SpotifyOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyOauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
